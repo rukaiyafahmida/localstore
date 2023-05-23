85d80b5e42382c14e6ca5e513f5357e283d3de19
rukaiyafahmida <rukaiyafahmida7@gmail.com>
2023-05-23 10:44:25 +0600

    initialize app store,tags

--- localstore/settings.py
+++ localstore/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "playground",
+    "debug_toolbar",
+    "store",
+    "tags"
 ]
 
 MIDDLEWARE = [
@@ -47,8 +50,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
-
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 ROOT_URLCONF = "localstore.urls"
 
 TEMPLATES = [
--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def say_hello(request):
+    x=1
+    y=2
     return render(request, 'hello.html', {'name' : 'Rukaiya'})
